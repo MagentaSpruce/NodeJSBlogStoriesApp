3c2a487b182d22e931dd02d2eb49d0393b33f652
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-18 18:37:13 -0400

    Setup Google oauth2.0 and passport.js GoogleStrategy

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -19,6 +25,22 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: "cool kids",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
