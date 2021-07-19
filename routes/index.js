c7ef495e722a57a04344559f1d19df49ddec6b61
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-19 10:45:46 -0400

    Protected routes, added greeting, handled errors
    The index.js routes were edited with the new MW for authenitcation. Sessions was used to prevent users from being logged out on refresh. The notes functionality was started using .lean() to link the template to server and error handling templates were made for 404 and 500 errors. Lastly, a loop was created to loop through all notes and output them to a table.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/authMW");
+const Word = require("../models/Words");
 
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   });
 });
 
-router.get("/dashboard", (req, res) => res.render("Dashboard"));
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const words = await Word.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      words,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
 module.exports = router;
